1a6700e40dfd8daba610fa8ad8bf5280cf695a7c
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2023-11-28 12:14:08 +0530

    foutreen commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import { json } from "express";
 import connectToMongoDB from "./conn.js";
 import routes from "./routes/routes.js";
 import dotenv from "dotenv";
+import { Server } from "http";
 
 dotenv.config();
 
@@ -13,13 +14,41 @@ const app = express();
 app.use(json());
 app.use("/", routes);
 
-const startServer = async () => {
-  await connectToMongoDB();
+let server;
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    console.log(`MongoDB connected at ${process.env.MONGODB_URI}`);
-  });
+const startServer = async () => {
+  try {
+    await connectToMongoDB();
+
+    server = app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+      console.log(`MongoDB connected at ${process.env.MONGODB_URI}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
+
+const shutdownServer = async () => {
+  if (server) {
+    await server.close(async () => {
+      console.log("Server closed");
+      await connectToMongoDB();
+    });
+  }
+};
+
+process.on("SIGINT", async () => {
+  console.log("Received SIGINT signal");
+  await shutdownServer();
+  process.exit(0);
+});
+
+process.on("uncaughtException", async (error) => {
+  console.error(error);
+  await shutdownServer();
+  process.exit(1);
+});
 
 startServer();
